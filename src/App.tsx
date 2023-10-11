abde662c3aee6d9ed71bde4748d4912100ba160a
GOLDER303 <szymonstepien@interia.pl>
2023-10-11 18:57:15 +0200

    Add basic main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-    return <h1> Hello World!</h1>
+    return (
+        <div className="pt-10 h-screen bg-gradient-to-r from-purple-700 to-purple-500 text-neutral-100">
+            <h1 className=" text-3xl text-center font-bold drop-shadow-lg">Rock Paper Scissors</h1>
+            <div className="flex h-5/6 justify-center items-center text-xl">
+                <button className="p-4 font-bold border-4 border-yellow-500 rounded-full shadow-xl">
+                    Play The Game
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default App
