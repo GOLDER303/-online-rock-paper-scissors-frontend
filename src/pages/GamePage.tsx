9a9a46becc436a5cb6898d5b702d6dbbe231be15
GOLDER303 <szymonstepien@interia.pl>
2023-10-18 15:59:51 +0200

    Add game page styling for larger screens

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -7,17 +7,17 @@ const GamePage = () => {
         <div className="flex flex-col h-screen pt-8 bg-gradient-to-r from-purple-700 to-purple-600 text-white">
             <h1 className="text-4xl text-center font-bold drop-shadow-lg">Round 1</h1>
 
-            <div className="flex flex-col flex-1">
-                <div className="flex flex-col justify-evenly items-center flex-1">
-                    <h2 className="text-center text-2xl font-bold">Opponent's Score: 0</h2>
-                    <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-orange-300"></div>
+            <div className="flex flex-col flex-1 lg:flex-row-reverse">
+                <div className="flex flex-col justify-evenly items-center flex-1 lg:justify-center">
+                    <h2 className="text-center text-2xl font-bold lg:mb-32 lg:text-3xl">Opponent's Score: 0</h2>
+                    <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-orange-300 lg:h-48 lg:w-48"></div>
                 </div>
 
-                <div className="flex flex-col justify-evenly flex-1">
-                    <h2 className="text-center text-2xl font-bold">Your Score: 0</h2>
+                <div className="flex flex-col justify-evenly flex-1 lg:justify-center">
+                    <h2 className="text-center text-2xl font-bold lg:mb-32 lg:text-3xl">Your Score: 0</h2>
 
-                    <div className="flex items-center justify-center gap-5">
-                        <div className="w-full">
+                    <div className="flex h-48 items-center justify-center gap-5 lg:justify-evenly">
+                        <div className="w-full max-w-[150px]">
                             {/* https://www.flaticon.com/free-icons/rock */}
                             <img
                                 src={rockIcon}
@@ -25,7 +25,7 @@ const GamePage = () => {
                             />
                             <p className="mt-2 text-2xl font-bold text-center">Rock</p>
                         </div>
-                        <div className="w-full">
+                        <div className="w-full max-w-[150px]">
                             {/* https://www.flaticon.com/free-icons/paper */}
                             <img
                                 src={paperIcon}
@@ -33,7 +33,7 @@ const GamePage = () => {
                             />
                             <p className="mt-2 text-2xl font-bold text-center">Paper</p>
                         </div>
-                        <div className="w-full">
+                        <div className="w-full max-w-[150px]">
                             {/* https://www.flaticon.com/free-icons/scissors */}
                             <img
                                 src={scissorsIcon}
