733d3a632966e84771cdd0a7236896e5e332f7e5
GOLDER303 <szymonstepien@interia.pl>
2023-10-24 18:05:51 +0200

    Fix waiting for opponent page styling on smaller screens

--- src/pages/GamePage/WaitingForOpponentPage.tsx
+++ src/pages/GamePage/WaitingForOpponentPage.tsx
@@ -11,19 +11,19 @@ const WaitingForOpponentPage = () => {
 
     return (
         <div className="flex h-screen flex-col justify-center items-center pt-8 bg-gradient-to-r from-purple-700 to-purple-600 text-white">
-            <h1 className="mb-40 text-4xl text-center font-bold drop-shadow-lg">
-                Waiting for the opponent to connect . . .
+            <h1 className="mb-20 px-4 text-3xl text-center font-bold drop-shadow-lg lg:text-4xl lg:mb-52">
+                Waiting for the opponent to connect <span className="whitespace-nowrap">. . .</span>
             </h1>
 
             <div
-                className="group relative mb-8 py-4 px-16 border-2 border-white rounded-lg cursor-pointer shadow-lg"
+                className="group relative mb-8 py-3 px-8 border-2 border-white rounded-lg cursor-pointer shadow-lg lg:px-16"
                 onClick={handleRoomUrlCopy}
             >
                 <div className="absolute top-0 left-0 w-full h-full bg-white opacity-25 group-hover:opacity-20"></div>
                 {!isUrlCopied ? (
-                    <p className="relative text-xl">{window.location.href}</p>
+                    <p className="relative text-lg text-center lg:text-xl">{window.location.href}</p>
                 ) : (
-                    <p className="relative text-xl">Copied!</p>
+                    <p className="relative text-lg text-center lg:text-xl">Copied!</p>
                 )}
             </div>
         </div>
